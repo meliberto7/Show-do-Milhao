0ed5836b1b0d0151f556ebf19ca696b3ee611e80
meliberto7 <130302288+meliberto7@users.noreply.github.com>
2024-08-03 19:54:14 -0300

    Nome no ranking

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -4,14 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author senai
- */
+
 public class Conexao {
     private static final String url = "jdbc:mysql://localhost:3306/quiz";
     private static final String usuario = "root";
-    private static final String senha = "1";
+    private static final String senha = "admeli";
   
   public static Connection conectar() throws SQLException {
       return (Connection) DriverManager.getConnection(url, usuario, senha);
--- src/model/dao/RankingDAO.java
+++ src/model/dao/RankingDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model.dao;
 
 import conexao.Conexao;
@@ -13,12 +9,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import model.bean.Ranking;
-import model.bean.Usuarios;
 
-/**
- *
- * @author Senai
- */
+
 public class RankingDAO {
     private String ranking;
     
